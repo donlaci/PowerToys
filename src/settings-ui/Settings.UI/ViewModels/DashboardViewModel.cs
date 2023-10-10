a3fedd3f878334c1d3e243c63feba5c51322e948
Laszlo Nemeth <laszlo.nemeth.hu@gmail.com>
2023-10-10 11:41:24 +0200

    Implementing 3rd variant for dashboard panel

--- src/settings-ui/Settings.UI/ViewModels/DashboardViewModel.cs
+++ src/settings-ui/Settings.UI/ViewModels/DashboardViewModel.cs
@@ -5,7 +5,9 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using global::PowerToys.GPOWrapper;
 using Microsoft.PowerToys.Settings.UI.Library;
 using Microsoft.PowerToys.Settings.UI.Library.Helpers;
@@ -19,11 +21,40 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 {
     public class DashboardViewModel : Observable
     {
+        private bool _isAddPanelVisible;
+        private string _addButtonGlyph;
+
         public Func<string, int> SendConfigMSG { get; }
 
         public ObservableCollection<DashboardListItem> ActiveModules { get; set; } = new ObservableCollection<DashboardListItem>();
 
         public ObservableCollection<DashboardListItem> AllModules { get; set; } = new ObservableCollection<DashboardListItem>();
+
+        public bool IsAddPanelVisible
+        {
+            get => _isAddPanelVisible;
+            set
+            {
+                if (_isAddPanelVisible != value)
+                {
+                    _isAddPanelVisible = value;
+                    OnPropertyChanged(nameof(IsAddPanelVisible));
+                }
+            }
+        }
+
+        public string AddButtonGlyph
+        {
+            get => _addButtonGlyph;
+            set
+            {
+                if (_addButtonGlyph != value)
+                {
+                    _addButtonGlyph = value;
+                    OnPropertyChanged(nameof(AddButtonGlyph));
+                }
+            }
+        }
 
         public bool UpdateAvailable { get; set; }
 
@@ -41,6 +72,9 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
 
             // set the callback functions value to hangle outgoing IPC message.
             SendConfigMSG = ipcMSGCallBackFunc;
+
+            IsAddPanelVisible = false;
+            AddButtonGlyph = "\uECC8";
 
             _allModules = new List<DashboardListItem>();
 
@@ -730,6 +764,20 @@ namespace Microsoft.PowerToys.Settings.UI.ViewModels
         {
             var actionName = "Launch";
             SendConfigMSG("{\"action\":{\"RegistryPreview\":{\"action_name\":\"" + actionName + "\", \"value\":\"\"}}}");
+        }
+
+        internal void AddButtonClick()
+        {
+            if (IsAddPanelVisible)
+            {
+                IsAddPanelVisible = false;
+                AddButtonGlyph = "\uECC8";
+            }
+            else
+            {
+                IsAddPanelVisible = true;
+                AddButtonGlyph = "\uE930";
+            }
         }
     }
 }
